ab1a09e768e338df297320fc64800e5188c72e4d
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2022-06-22 22:35:20 +0100

    Init project and add colours

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+</script>
+
+<main>
+	testing
+
+	<p class="red">testing</p>
+	<p class="orange">testing</p>
+	<p class="green">testing</p>
+	<p class="turquoise">testing</p>
+	<p class="blue">testing</p>
+	<p class="purple">testing</p>
+</main>
+
+<style lang="sass">
+	:root
+		background: #0e1628
+		color: white
+		font-family: "Courier New"
+
+	.red
+		color: #f06
+	.orange
+		color: #f60
+	.green
+		color: #6f0
+	.turquoise
+		color: #06989a
+	.blue
+		color: #3465a4
+	.purple
+		color: #75507b
+</style>
